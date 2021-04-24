bba6ccd23659c67266f2973bc4a7ef7528804760
Dan Edington <dan2505@hotmail.com>
2021-04-25 00:35:50 +0100

    added defaultCube sketch

--- src/index.js
+++ src/index.js
@@ -1,49 +1,6 @@
-import * as THREE from 'three';
-import * as dat from 'dat.gui';
-
 import Sketch from './Sketch/Sketch';
-
+import defaultCube from './sketches/defaultCube';
 import './index.css';
 
-const test = function () {
-    let cube, lights;
-
-    const gui = new dat.GUI();
-
-    const vars = {
-        rotation: 0,
-    };
-
-    const createCube = function () {
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshStandardMaterial( { color: 'hotpink' } );
-        return new THREE.Mesh(geometry, material);  
-    }
-    
-    const createLights = function () {
-        return {
-            ambient: new THREE.AmbientLight(0xffffff, 0.25),
-            point: new THREE.PointLight(0xffffff, 1.0),
-        }
-    }
-
-    const setup = () => {
-        gui.add(vars, 'rotation', 0, Math.PI * 2, 0.001);
-        lights = createLights();
-        lights.point.position.set(2, 2, 2);
-        cube = createCube();
-        this.scene.add(cube, ...Object.values(lights));
-    };
-
-    const onFrame = () => {
-        cube.rotation.y = vars.rotation;
-    };
-
-    return {
-        setup,
-        onFrame,
-    }
-}
-
-const s = new Sketch(test);
+const s = new Sketch(defaultCube);
 s.start();
---
+++ src/sketches/defaultCube.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+export default function () {
+  let cube, lights;
+
+  const gui = new dat.GUI();
+
+  const vars = {
+      rotation: 0,
+  };
+
+  const createCube = function () {
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshStandardMaterial( { color: 'hotpink' } );
+      return new THREE.Mesh(geometry, material);  
+  }
+  
+  const createLights = function () {
+      return {
+          ambient: new THREE.AmbientLight(0xffffff, 0.25),
+          point: new THREE.PointLight(0xffffff, 1.0),
+      }
+  }
+
+  const setup = () => {
+      gui.add(vars, 'rotation', 0, Math.PI * 2, 0.001);
+      lights = createLights();
+      lights.point.position.set(2, 2, 2);
+      cube = createCube();
+      this.scene.add(cube, ...Object.values(lights));
+  };
+
+  const onFrame = () => {
+      cube.rotation.y = vars.rotation;
+  };
+
+  return {
+      setup,
+      onFrame,
+  }
+}
