b53cdf5005fa832ac7401f1b17e3271099da7a49
Dan Edington <dan2505@hotmail.com>
2021-04-24 13:31:39 +0100

    added antialiasing and correct pixel ratio

--- src/ResizeHandler.js
+++ src/ResizeHandler.js
@@ -2,4 +2,5 @@ export default function() {
   this.camera.aspect = window.innerWidth / window.innerHeight
   this.camera.updateProjectionMatrix();
   this.renderer.setSize(window.innerWidth, window.innerHeight);
+  this.renderer.setPixelRatio(window.devicePixelRatio);
 }
--- src/SetupRenderer.js
+++ src/SetupRenderer.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
 
 export default function () {
-  this.renderer = new THREE.WebGLRenderer();
+  this.renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
   this.renderer.setSize(window.innerWidth, window.innerHeight);
+  this.renderer.setPixelRatio(window.devicePixelRatio);
   this.renderer.setClearColor(0xc6c6c6);
   document.body.appendChild(this.renderer.domElement);
 }
