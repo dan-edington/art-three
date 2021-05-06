93b5f8e870c5fb5cef5015b3be05648a6b93e462
Dan Edington <dan.edington@deptagency.com>
2021-05-06 17:28:15 +0100

    adds shouldRender to resize handler

--- src/Sketch/ResizeHandler.ts
+++ src/Sketch/ResizeHandler.ts
@@ -8,4 +8,5 @@ export default function (this: SketchClass): void {
   this.camera.updateProjectionMatrix();
   this.renderer.setSize(window.innerWidth, window.innerHeight);
   this.renderer.setPixelRatio(window.devicePixelRatio);
+  this.shouldRender = true;
 }
