e20cafd3c9c4fc038ac43ff489881e08c82ee4eb
Dan Edington <dan2505@hotmail.com>
2021-05-08 03:33:14 +0100

    adds basic physics

--- src/sketches/waterwheels/waterwheels.ts
+++ src/sketches/waterwheels/waterwheels.ts
@@ -3,22 +3,28 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import { SketchObject, SketchClass } from '../../types/sketch';
 
+interface Wheel {
+  wheel: THREE.Group;
+  wheelBody: CANNON.Body;
+}
+interface Ball {
+  ball: THREE.Mesh;
+  ballBody: CANNON.Body;
+}
 interface LightsObject {
   [key: string]: THREE.Light;
 }
 
-interface Wheels {
-  group: THREE.Group;
-  xyPos: THREE.Vector2;
-}
-
 export default function (this: SketchClass): SketchObject {
   let lights: LightsObject;
-  let wheels: Wheels[];
+  let wheels: Wheel[];
+  let balls: Ball[];
   const wheelCount = 5;
+  const wheelSides = 8;
   const dummyMatrix = new THREE.Object3D();
+
   const world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+    gravity: new CANNON.Vec3(0, 9.82, 0), // m/s²
   });
 
   const addLights = (): LightsObject => {
@@ -28,39 +34,74 @@ export default function (this: SketchClass): SketchObject {
     };
   };
 
-  const createWheel = (): [] => {
-    const wheel = new THREE.Group();
+  const getBladeTransform = (i: number) => {
+    const ii = i + Math.PI * 0.8;
+    const radius = 1.4;
+    const step = (Math.PI * 2) / wheelSides;
+    const x = radius * Math.sin(step * ii);
+    const y = radius * Math.cos(step * ii);
+    const rotationY = step * ii;
+    return {
+      x,
+      y,
+      rotationY,
+    };
+  };
 
+  const createWheel = (position: THREE.Vector2, scale: number): Wheel => {
+    const wheel = new THREE.Group();
     const material = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
+    const wheelBody = new CANNON.Body({ mass: 0 });
+
+    // center
 
     const center = new THREE.Mesh(
-      new THREE.CylinderBufferGeometry(1, 1, 0.5, 8, 1, false),
+      new THREE.CylinderBufferGeometry(1 * scale, 1 * scale, 0.5 * scale, wheelSides, 1, false),
       material,
     );
 
-    const centerShape = new CANNON.Cylinder(1, 1, 0.5, 8);
-    const centerBody = new CANNON.Body({ mass: 1, shape: centerShape });
-
-    const blade = new THREE.InstancedMesh(
-      new THREE.BoxBufferGeometry(0.1, 0.5, 1, 1, 1, 1),
-      material,
-      8,
-    );
-
-    const bladeShape = new CANNON.Box(new CANNON.Vec3(0.1, 0.5, 1));
-    const bladeBodies = [];
-
-    for (let i = 0; i < 8; i++) {
-      const bladeBody = new CANNON.Body({ mass: 1, shape: bladeShape });
-      bladeBodies.push(bladeBody);
-    }
-
-    blade.rotation.x = Math.PI * 0.5;
-    center.rotation.x = Math.PI * 0.5;
-    wheel.add(blade);
     wheel.add(center);
 
-    return [wheel, { centerBody, bladeBodies }];
+    const centerShape = new CANNON.Cylinder(1 * scale, 1 * scale, 0.5 * scale, wheelSides);
+    wheelBody.addShape(
+      centerShape,
+      new CANNON.Vec3(0, 0, 0),
+      // @ts-ignore
+      new CANNON.Quaternion().copy(center.quaternion),
+    );
+
+    // blades
+
+    const bladeGeometry = new THREE.BoxBufferGeometry(0.1 * scale, 0.5 * scale, 1 * scale, 1, 1, 1);
+
+    for (let i = 0; i < wheelSides; i++) {
+      const { x, y, rotationY } = getBladeTransform(i);
+      const blade = new THREE.Mesh(bladeGeometry, material);
+      blade.position.set(x * scale, 0, y * scale);
+      blade.rotation.y = rotationY;
+      wheel.add(blade);
+
+      const bladeShape = new CANNON.Box(new CANNON.Vec3(0.1 * scale, 0.5 * scale, 1 * scale));
+      wheelBody.addShape(
+        bladeShape,
+        // @ts-ignore
+        new CANNON.Vec3(0, 0, 0).copy(blade.position),
+        // @ts-ignore
+        new CANNON.Quaternion().copy(blade.quaternion),
+      );
+    }
+
+    wheel.rotation.x = Math.PI * 0.5;
+    wheel.position.set(position.x, position.y, 0);
+    // @ts-ignore
+    wheelBody.quaternion.copy(wheel.quaternion);
+    // @ts-ignore
+    wheelBody.position.copy(wheel.position);
+
+    return {
+      wheel,
+      wheelBody,
+    };
   };
 
   const createWall = () => {
@@ -69,19 +110,36 @@ export default function (this: SketchClass): SketchObject {
     return wall;
   };
 
-  const createRandomWheels = (): Wheels[] => {
+  const createRandomWheels = () => {
     const wheels = [];
+
     for (let i = 0; i < wheelCount; i++) {
-      const [wheel, wheelBodies] = createWheel();
       const xyPos = new THREE.Vector2(Math.random() * 10 - 5, i * 2 - (wheelCount - 1));
-      wheel.scale.set(0.25, 0.25, 0.25);
-      wheel.position.set(xyPos.x, xyPos.y, 0);
-      wheels.push({
-        group: wheel,
-        xyPos,
+      const wheel = createWheel(xyPos, 0.25);
+      wheels.push(wheel);
+    }
+
+    return wheels;
+  };
+
+  const createBalls = (): Ball[] => {
+    const radius = 0.05;
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ffff });
+    const ballShape = new CANNON.Sphere(radius);
+
+    const balls = [];
+
+    for (let i = 0; i < 50; i++) {
+      const ball = new THREE.Mesh(new THREE.SphereBufferGeometry(radius, 8, 8), material);
+      const ballBody = new CANNON.Body({ mass: 1 });
+      ballBody.addShape(ballShape);
+      balls.push({
+        ball,
+        ballBody,
       });
     }
-    return wheels;
+
+    return balls;
   };
 
   const setup = (): void => {
@@ -95,34 +153,52 @@ export default function (this: SketchClass): SketchObject {
     const wall = createWall();
     wall.position.z = -0.5;
 
+    balls = createBalls();
+    balls.forEach((b) => {
+      b.ball.position.y = -5;
+      b.ball.position.x = Math.random() * 10 - 5;
+      // @ts-ignore
+      b.ballBody.position.copy(b.ball.position);
+      this.scene.add(b.ball);
+      world.addBody(b.ballBody);
+    });
+
     wheels = createRandomWheels();
     wheels.forEach((w) => {
-      this.scene.add(w.group);
+      this.scene.add(w.wheel);
+      world.addBody(w.wheelBody);
     });
 
     this.scene.add(wall, ...Object.values(lights));
   };
 
-  const renderInstancedBlades = () => {
-    wheels.forEach((w) => {
-      for (let i = 0; i < 8; i++) {
-        const ii = i + Math.PI * 0.8;
-        const radius = 1.4;
-        const step = (Math.PI * 2) / 8;
-        const x = radius * Math.sin(step * ii);
-        const y = radius * Math.cos(step * ii);
-        dummyMatrix.position.set(x, 0, y);
-        dummyMatrix.rotation.y = step * ii;
-        dummyMatrix.updateMatrix();
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        w.group.children[0].setMatrixAt(i, dummyMatrix.matrix);
-      }
-    });
-  };
+  const timeStep = 1 / 60;
+  let lastCallTime: number;
 
   const onFrame = (): void => {
-    renderInstancedBlades();
+    const time = performance.now() / 1000; // seconds
+    if (!lastCallTime) {
+      world.step(timeStep);
+    } else {
+      const dt = time - lastCallTime;
+      world.step(timeStep, dt);
+    }
+    lastCallTime = time;
+
+    wheels.forEach((w) => {
+      // @ts-ignore
+      w.wheel.position.copy(w.wheelBody.position);
+      // @ts-ignore
+      w.wheel.quaternion.copy(w.wheelBody.quaternion);
+    });
+
+    balls.forEach((b) => {
+      // @ts-ignore
+      b.ball.position.copy(b.ballBody.position);
+      // @ts-ignore
+      b.ball.quaternion.copy(b.ballBody.quaternion);
+    });
+
     this.shouldRender = true;
   };
 
