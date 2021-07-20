7d16236b9c4fef2a6344ee977ad9075f0c99845a
Dan Edington <dan2505@hotmail.com>
2021-07-20 21:05:05 +0100

    add emission materials

--- src/sketches/portal/portal.ts
+++ src/sketches/portal/portal.ts
@@ -21,23 +21,46 @@ export default function (this: SketchClass): SketchObject {
     const texture = await new Promise((resolve) => {
       new THREE.TextureLoader().load('./bake.jpg', (t) => {
         t.flipY = false;
+        t.encoding = THREE.sRGBEncoding;
         resolve(t);
       });
     });
+
+    const lampMaterial = new THREE.MeshBasicMaterial({ color: 0xfffffc });
+    const portalMaterial = new THREE.MeshBasicMaterial({ color: 0xe4fdff, side: THREE.DoubleSide });
 
     await new Promise((resolve) => {
       new GLTFLoader().load('./scene.glb', (gltf) => {
         gltf.scene.traverse((child) => {
           child.material = new THREE.MeshBasicMaterial({ map: texture });
         });
+
+        const emissionPortal = gltf.scene.children.find((child) => {
+          return child.name === 'emissionPortal';
+        });
+
+        const emissionStreetLamp1 = gltf.scene.children.find((child) => {
+          return child.name === 'emissionStreetLamp1';
+        });
+
+        const emissionStreetLamp2 = gltf.scene.children.find((child) => {
+          return child.name === 'emissionStreetLamp2';
+        });
+
+        emissionPortal?.material = portalMaterial;
+        emissionStreetLamp1?.material = lampMaterial;
+        emissionStreetLamp2?.material = lampMaterial;
+
         this.scene.add(gltf.scene);
+
         resolve();
       });
     });
 
     lights = createLights();
     this.scene.add(...Object.values(lights));
-    this.renderer.setClearColor(0x000000);
+    this.renderer.setClearColor(0x464646);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
   };
 
   const onFrame = () => {
