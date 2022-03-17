23448a69ca539f2d299076b0d6326894f7121613
Dan Edington <dan2505@hotmail.com>
2022-03-17 12:57:36 +0000

    update comp collagism code and images

--- src/sketches/computational-collagism/computational-collagism.ts
+++ src/sketches/computational-collagism/computational-collagism.ts
@@ -4,40 +4,22 @@ import { SketchP5ArtworkFunction } from '../../types/sketchP5';
 
 /*
 SEEDS:
-1234552636658
-704533665443
-155886698777
-1469267824587
-185552810222
-568302445769
-504583409659
-1353508536524
-739184037602
-765750511181
-1251325995349
-1393757821946
-1593487533796
-1212863139133
-1618151481409
-85196780735
-1288887673963
-218334494909
-987230861321
-1550353129644
-515991930439
-629628517245
+174616579851
+24876316212
+903006744114
+798710989898
+896894337073
+719219509155
+839051658578
+848984868122
+267024133558
+1528171703215
 */
 
 const artwork = (seed: number) => (p5: P5): void => {
-  const pallete = [
-    p5.color('#255AB5'),
-    p5.color('#F7D648'),
-    p5.color('#000000'),
-    p5.color('#dfdfdf'),
-  ];
+  const pallete = [p5.color('#255AB5'), p5.color('#F7D648'), p5.color('#dfdfdf')];
 
   const tintPallete = [pallete[0], pallete[1]];
-  const bgPallete = [pallete[2], pallete[3]];
 
   const fonts: Array<P5.Font> = [];
   const fontVariants = [
@@ -52,31 +34,19 @@ const artwork = (seed: number) => (p5: P5): void => {
     'Regular',
   ];
 
-  const imageCount = 6; //13;
+  const imageCount = 10;
   const images: Array<P5.Image> = [];
   let rows: Array<number>, cols: Array<number>;
 
-  const size = 900;
-  const blankChance = 0.4;
-  const textChance = 0;
   let textDrawn = false;
+  const size = 900;
+  const blankChance = 0.8;
+  const textChance = 0.75;
   const axisPiecesMax = 6;
   const gridCols = 12;
   const imageSourceOffsetRange = 100;
-  const sizeOffsetRange = Math.floor(size / axisPiecesMax);
-  const scaleMin = 0.3;
-  const overlayMaxOpacity = 0.5;
 
   const generateGridAxis = (): Array<number> => {
-    // const averageSize = size / numberOfPieces;
-    // let difference = 0;
-    // for (let i = 0; i < numberOfPieces; i++) {
-    //   const sizeOffset = Math.floor(p5.random(-sizeOffsetRange, sizeOffsetRange));
-    //   const size = averageSize + sizeOffset;
-    //   difference += size - averageSize;
-    //   gridAxis.push(size);
-    // }
-    // gridAxis[numberOfPieces - 1] = gridAxis[numberOfPieces - 1] - difference;
     const oneCol = 900 / gridCols;
     const colSpanCount = Math.floor(p5.random(1, axisPiecesMax));
     const maxInitialColSpanSize = Math.floor(gridCols / colSpanCount);
@@ -119,25 +89,21 @@ const artwork = (seed: number) => (p5: P5): void => {
       leading = p5.map(textSize, 100, 200, 80, 150);
     }
 
-    if (color === pallete[0] || color === pallete[2]) {
-      offscreen.fill(pallete[p5.random() > 0.5 ? 1 : 3]);
-    } else {
-      offscreen.fill(pallete[p5.random() > 0.5 ? 2 : 0]);
-    }
+    const possiblePallete = pallete.filter((c) => c != color);
 
     const font = fonts[Math.floor(p5.random(0, fonts.length))];
 
     offscreen.noStroke();
+    offscreen.fill(p5.color(p5.random(possiblePallete)));
     offscreen.textSize(textSize);
     offscreen.textLeading(leading);
     offscreen.textAlign(p5.RIGHT, p5.BOTTOM);
     offscreen.textWrap(p5.CHAR);
     offscreen.textFont(font);
-    console.log(font);
     offscreen.text('PEACE', 0, 0, width + textSize * 0.03, height + textSize * 0.23);
-    textDrawn = true;
     p5.blendMode(p5.BLEND);
     p5.image(offscreen, xPos, yPos);
+    textDrawn = true;
   };
 
   const drawCollagePiece = (
@@ -160,8 +126,12 @@ const artwork = (seed: number) => (p5: P5): void => {
       offscreen.image(selectedImage, 0, 0, width, height, sx, sy, width / scale, height / scale);
       offscreen.filter(p5.GRAY);
 
+      p5.noStroke();
+      p5.fill(color);
+      p5.rect(xPos, yPos, width, height);
       p5.tint(color);
       p5.image(offscreen, xPos, yPos, width, height, 0, 0);
+      p5.tint(255);
     } else {
       p5.noStroke();
       p5.fill(color);
@@ -193,8 +163,6 @@ const artwork = (seed: number) => (p5: P5): void => {
   };
 
   p5.draw = function () {
-    p5.background(bgPallete[Math.floor(p5.random(0, bgPallete.length))]);
-
     let xPos = 0;
     let yPos = 0;
 
@@ -205,7 +173,7 @@ const artwork = (seed: number) => (p5: P5): void => {
         if (img) {
           color = tintPallete[Math.floor(p5.random(tintPallete.length))];
         } else {
-          color = pallete[Math.floor(p5.random(pallete.length))];
+          color = tintPallete[Math.floor(p5.random(tintPallete.length))];
         }
         drawCollagePiece(img, color, xPos, yPos, cols[j], rows[i]);
         xPos += cols[j];
